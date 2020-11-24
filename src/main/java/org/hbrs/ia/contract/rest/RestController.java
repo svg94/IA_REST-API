9ecd1a8e7cdd96fbab21dfd6d83e8f871c5f6935
svg94 <liga-2000@hotmail.de>
2020-11-24 16:07:12 +0100

    Initialise

---
+++ src/main/java/org/hbrs/ia/contract/rest/RestController.java
@@ -0,0 +1,11 @@
+package org.hbrs.ia.contract.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestController {
+    public static void main(String[] args){
+        SpringApplication.run(RestController.class, args);
+    }
+}
